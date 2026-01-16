8ebe512beee563e256a89363a5d3b4d7d721ce6f
soemyatmaw1998 <soemyatmaw1998@gmail.com>
2026-01-16 18:52:35 +0900

    Create EmployeeTable.sql

---
+++ EmployeeTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    salary DECIMAL(10,2)
+);
+
+CREATE TABLE Department (
+    id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
